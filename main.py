1eaeaca6058616c2f704cfeed73366cb189330ad
Jose Ortiz <joseortizgomez90@gmail.com>
2023-04-30 11:15:06 +0200

    cambios

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,g
 import pickle
+import psycopg2
 
 # Instanciamos la clase Flask
 app = Flask(__name__)
@@ -33,7 +34,45 @@ def predictions():
     prediction = loaded_model.predict([new_data])
     # Retornamos la predicción en formato JSON
     return jsonify({'prediction': prediction[0]})
+
+
+# 2.Ruta para añadir datos
+
+@app.route('/api/v1/retrain', methods=['POST'])
+def add_datos():
+    # Obtener datos del cuerpo de la petición
+    new_data = request.get_json()
+
+    # Almacenar los datos en variables
+    TV = new_data['TV']
+    radio = new_data['radio']
+    newspaper =['newspaper']
+    sales= new_data['sales']
+   
+
+    # Crear la conexión a la base de datos
+    conn = get_Adverstising.csv()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Adverstising.csv (TV,radio,newspaper,sales) VALUES (%s, %s, %s, %s, %s)", (TV,radio,newspaper,sales))
+    conn.commit()
+    cursor.close()
+
+    return jsonify({'message': 'los datos han sido añadidos correctamente'})
+
+# Realizamos la predicción
+    new_prediction = loaded_model.predict([add_datos])
+    # Retornamos la predicción en formato JSON
+    return jsonify({'prediction': prediction[0]})
+
     
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+    
+    
+    
+    
+    
+  
